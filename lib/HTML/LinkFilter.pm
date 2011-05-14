39c52b572f9c36b5bc84dd33ee2a86ee784cbde1
Kuniyoshi Kouji <kuniyoshi@cpan.org>
2011-05-14 14:46:10 +0900

    initial commit

---
+++ lib/HTML/LinkFilter.pm
@@ -0,0 +1,32 @@
+package HTML::LinkFilter;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+HTML::LinkFilter -
+
+=head1 SYNOPSIS
+
+  use HTML::LinkFilter;
+
+=head1 DESCRIPTION
+
+HTML::LinkFilter is
+
+=head1 AUTHOR
+
+kuniyoshi kouji E<lt>kuniyoshi@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
